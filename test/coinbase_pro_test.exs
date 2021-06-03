17d20847bd9b065847b74bc8d1f40dc7b3b9d082
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2021-06-03 16:03:45 +0300

    feat: mocking is not working

--- test/coinbase_pro_test.exs
+++ test/coinbase_pro_test.exs
@@ -2,6 +2,20 @@ defmodule CoinbaseProTest do
   use ExUnit.Case
   doctest CoinbasePro
 
+  import Tesla.Mock
+
+  setup do
+    mock(fn
+      %{method: :get, url: "http://example.com/hello"} ->
+        %Tesla.Env{status: 200, body: "hello"}
+
+      %{method: :post, url: "http://example.com/world"} ->
+        json(%{"my" => "data"})
+    end)
+
+    :ok
+  end
+
   test "greets the world" do
     assert CoinbasePro.products() == :world
   end
