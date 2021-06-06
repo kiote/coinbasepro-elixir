7145404410d32042caa4ec30ac3fc89d3b136783
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2021-06-06 17:30:10 +0300

    fix: coveralls

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule CoinbasePro.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
       deps: deps()
     ]
   end
