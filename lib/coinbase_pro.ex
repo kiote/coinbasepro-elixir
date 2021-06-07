fdd34eed3ba147e2176808a8e7919a01d271dcc9
kiote <krivich.ekaterina@gmail.com>
2021-06-07 11:48:03 +0300

    fix: credo

--- lib/coinbase_pro.ex
+++ lib/coinbase_pro.ex
@@ -3,15 +3,15 @@ defmodule CoinbasePro do
   General API
   """
 
-  defp api_path() do
+  defp api_path do
     Application.get_env(:coinbase_pro, :api_path, "localhost:8080")
   end
 
-  defp http_client() do
+  defp http_client do
     CoinbasePro.Http.Poison
   end
 
-  def products() do
+  def products do
     (api_path() <> "/products")
     |> http_client().get()
   end
