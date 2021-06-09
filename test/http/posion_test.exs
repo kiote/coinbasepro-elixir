284e0b9d54da35e096fc86bdb0245a0cb0f1c08e
kiote <krivich.ekaterina@gmail.com>
2021-06-09 19:48:35 +0300

    fix: more tests

--- test/http/posion_test.exs
+++ test/http/posion_test.exs
@@ -2,11 +2,19 @@ defmodule CoinbasePro.Http.PoisonTest do
   use ExUnit.Case
   import CoinbasePro.Http.Poison
 
-  test "gets url" do
-    assert [_ | _] = get("http://localhost:8080/products")
+  describe "get" do
+    test "gets url" do
+      assert [_ | _] = get("http://localhost:8080/products")
+    end
+
+    test "failed response" do
+      assert {:error, _} = get("http://localhost")
+    end
   end
 
-  test "failed response" do
-    assert {:error, _} = get("http://localhost")
+  describe "post" do
+    test "failed" do
+      assert {:error, _} = post("http://localhost", Jason.encode!(%{}))
+    end
   end
 end
